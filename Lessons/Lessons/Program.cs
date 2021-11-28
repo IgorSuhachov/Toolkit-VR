4767439641220751d59f1cb70c11ebc2e6941170
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2021-11-28 23:13:57 +0300

    update

---
+++ Lessons/Lessons/Program.cs
@@ -0,0 +1,9 @@
+﻿{
+    Console.WriteLine("Первый блок");
+    Console.WriteLine("1");
+    Console.WriteLine("2");
+    {
+        Console.WriteLine("Второй блок");
+        Console.WriteLine("1");
+    }
+}
