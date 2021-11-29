a714694bec1a103acd75d3613d166aab85584483
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2021-11-29 13:06:41 +0300

    Дополнение

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 12;
+            //double price = 12.2;
+            //string name = "Igor";
+
+
+            //Console.WriteLine("Введите число");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Введите цену");
+            //double price = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите имя");
+            //string name = Console.ReadLine();
+
+            int a = 12;
+            int b = 2;
+            int c = a + b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
