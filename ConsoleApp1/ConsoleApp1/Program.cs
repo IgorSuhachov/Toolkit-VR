f40cd11185da0cba3ffe129ceb1c84208e2bf59e
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2021-11-30 14:22:54 +0300

    3 урок

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -20,11 +20,44 @@ namespace ConsoleApp1
             //Console.WriteLine("Введите имя");
             //string name = Console.ReadLine();
 
-            int a = 12;
-            int b = 2;
-            int c = a + b;
-            Console.WriteLine(c);
+            //int a = 12;
+            //int b = 2;
+            //int c = a + b;
+            //Console.WriteLine(c);
 
+            //int b = 5;
+            //b = b + 5;
+            //b = b + b;
+            //int a = b - 2;
+            //Console.WriteLine(a);
+
+            //int number = 12;
+            //double number1 = 12,2;
+            //number = number + number1;
+
+            //int a, b, c;
+            //a = b = c = 12;
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    Console.WriteLine("Число " + a + " больше, чем " + b);
+            //}
+
+
+            double a = 1;
+            double b = 2.1;
+
+            if (a == 1.2 && b == 2.1)
+            {
+                Console.WriteLine("s");
+            }
+            else
+            {
+                Console.WriteLine("a");
+            }
         }
     }
 }
