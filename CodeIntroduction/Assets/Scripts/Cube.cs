a4216cf14566694b1a78c9b4745df904e08409f8
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2022-02-01 22:52:46 +0300

    xz

---
+++ CodeIntroduction/Assets/Scripts/Cube.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float sizeModifier = 2.5f;
+    public string newName = "OrangeCube";
+    public bool isRotated = false;
+    
+    void Start()
+    {
+        transform.name = ImproveName(newName);
+        
+        if (true)
+        {
+            
+        }
+    }
+
+    string ImproveName (string originalString)
+    {
+        return "-[" + originalString + "]-"; 
+    }
+
+    void Update()
+    {
+        transform.localScale = Vector3.one * sizeModifier;
+    }
+}
